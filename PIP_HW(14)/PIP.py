7cd533cef7b797c4c321952043801b9a26d056e6
Javohir <javohirismoilov81@gmail.com>
2025-10-08 15:08:42 +0500

    .

---
+++ PIP_HW(14)/PIP.py
@@ -0,0 +1,31 @@
+from os.path import split
+
+import requests
+from bs4 import BeautifulSoup
+
+sahifa = "https://www.w3schools.com/python/python_pip.asp"
+r = requests.get(sahifa)
+
+soup = BeautifulSoup(r.text,'html.parser')
+news = soup.find_all(class_="w3-example")
+print(news[0].text)
+
+
+
+# №2    \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+
+
+
+link= "https://www.computerhistory.org/tdih/September/09/?utm_source=chatgpt.com"
+box = requests.get(link)
+
+quti = BeautifulSoup(box.text,'html.parser')
+matin = quti.find_all(class_="chm-tdih-entry-content")
+
+matn= matin[0].text
+
+jumlalar =matn.split('.')
+
+for jumla in jumlalar:
+    if jumla.strip():
+        print(jumla.strip()+'.')
